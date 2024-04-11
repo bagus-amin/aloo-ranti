520cb67eb8796caf630a313ace6b7fd71d8c4cb6
bagus-amin <146147218+bagus-amin@users.noreply.github.com>
2024-04-12 02:33:48 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,19 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
-    btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
-    btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    titleWeb: "HALOOO",
+    introTitle: 'Selamat Pagiii Deryanti Amaliah',
+    introDesc: `hihi baru bangunn yaaa? yaudaa ke kamar mandii dluu truss sarapan M...maksudnya teh abis dari kamar mandi sarapannya bukan sarapan di kamar mandi yeuuu
+    kalo udaaa ada yang mau bam omongin nihhh mau lihat??`,
+    btnIntro: 'mauuu dong bam sayang..',
+    title: 'Assikk di panggil sayang hihihi askdskasjd 🥰',
+    desc: 'tetap semangat ya rantii seberat apapun masalah yang ranti punya sekarang, walaupun ranti gabisa ceritain ke bam gimana capeknya, apa yang ranti alami, ranti harus tetap semangat. Bam tau kok ranti anak yang kuat walaupun kadang ranti masih belum ikhlas sepenuhnya kadang masih suka keinget / malah muncul lagi rasa trauma ranti,tapi ranti harus sabar yaa bam ga maksa ko buat disukain balik sama ranti biarkan rasa ini yang abis dengan sendirinya hehe, oh iya jangan sungkan kalo mau cerita masalah ranti cerita ajahh bam tarik kata-kata bam tentang jangan cerita tentang masa lalu ranti hehe semua boleehh kok sekarang mungkin bam bisa bantu/ kasi saran ke ranti,memang sii status kita belum ada apa" tapi soal semalem yang udah ranti putusin itu bikin bam selangkah kedepan buat deketin rantii hihi, maaf ya bam cuma bisa ngasih support dari jauh, maaf juga kalo bikin ranti kesel hehehe, maaf juga klo bam ga bisa effort ke ranti seperti masa lalu rantii. INTINYA SEMANGATT BUAT JALANI HARI INI YAHHH. oh iya bam ganteng ga sii? ',
+    btnYes: 'b ajah',
+    btnNo: 'iya bam ganteng',
+    question: 'huhu iyaa bam emng b ajah gantengan devano ceunahhh :" ya kan?? Semisal bam jadi cowok ranti apa yang bakal mau ranti lakuin pertama kali kita jadian?? terus juga kenapa ranti pilih keputusan yang ranti pilih?',
+    btnReply: 'itu bam',
+    reply: 'makasii cantikkk😘😘',
     mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
     messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
 }
+    
